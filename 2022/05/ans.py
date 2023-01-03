6e31296106d660aeecf59823835d4679e109b36f
Adil-Iqbal <main@adil-iqbal.com>
2023-01-02 23:14:21 -0600

    day5p1 done

--- 2022/05/ans.py
+++ 2022/05/ans.py
@@ -1,47 +1,64 @@
+import re
 import argparse
 
 
-def solve1(pairs):
-    ans = 0
-    for pair in pairs:
-        first, second = pair
-        min_f, max_f = first
-        min_s, max_s = second
-        first_contained = min_f <= min_s and max_f >= max_s
-        second_contained = min_s <= min_f and max_s >= max_f
-        if first_contained or second_contained:
-            ans += 1
+def solve1(content):
+    stacks, instructions = content
+    for moves in instructions:
+        qty, src, dest = moves
+        for _ in range(qty):
+            stacks[dest].append(stacks[src].pop())
+
+    ans = ""
+    for stack in stacks:
+        ans += stack.pop()
     return ans
 
 
-def solve2(pairs):
-    ans = 0
-    for pair in pairs:
-        first, second = pair
-        min_f, max_f = first
-        min_s, max_s = second
-        first_min_overlaps = min_f in range(min_s, max_s + 1)
-        first_max_overlaps = max_f in range(min_s, max_s + 1)
-        second_min_overlaps = min_s in range(min_f, max_f + 1)
-        second_max_overlaps = max_s in range(min_f, max_f + 1)
+def solve2(content):
+    print("TODO! Make this work!")
+    stacks, instructions = content
+    for moves in instructions:
+        qty, src, dest = moves
+        for _ in range(qty):
+            stacks[dest].append(stacks[src].pop())
 
-        if (
-            first_min_overlaps
-            or first_max_overlaps
-            or second_min_overlaps
-            or second_max_overlaps
-        ):
-            ans += 1
+    ans = ""
+    for stack in stacks:
+        ans += stack.pop()
     return ans
 
 
 def parse_file(file):
-    contents = file.read()
-    ass_pairs = contents.split("\n")
-    ass_pair_split = [pair.split(",") for pair in ass_pairs]
-    return [
-        tuple([tuple(int(n) for n in s.split("-")) for s in p]) for p in ass_pair_split
-    ]
+    stacks = []
+    instructions = []
+    instruction_numeric_ptn = re.compile(r"move (\d+) from (\d+) to (\d+)")
+    stackitem_ptn = re.compile(r"[A-Z]")
+    stacklimit_ptn = re.compile(r"^[\s\d]+$")
+    lines = file.read().split("\n")
+
+    stacklimit_idx = 0
+    for idx, line in enumerate(lines):
+        if re.search(stacklimit_ptn, line):
+            stacklimit_idx = idx
+
+    num_stacks = int(lines[stacklimit_idx][-2])
+    for _ in range(num_stacks):
+        stacks.append([])
+
+    for idx in reversed(range(stacklimit_idx)):
+        line = lines[idx]
+        stackcycle_idx = 0
+        for i in range(0, len(line) - 1, 4):
+            if m := re.search(stackitem_ptn, line[i : i + 4]):
+                stacks[stackcycle_idx].append(m.group(0))
+            stackcycle_idx += 1
+
+    for i in range(stacklimit_idx + 2, len(lines)):
+        m = re.match(instruction_numeric_ptn, lines[i])
+        instructions.append((int(m[1]), int(m[2]) - 1, int(m[3]) - 1))
+
+    return stacks, instructions
 
 
 def main():
